3abeb900d9589241df7290e0371930323b5f57c0
Anton klimenka <ieasycat@hotmail.com>
2021-04-06 22:27:23 +0300

    project

--- link_shortening/tests.py
+++ link_shortening/tests.py
@@ -1,3 +1,29 @@
-# from django.test import TestCase
+from django.test import TestCase
+from link_shortening.views import reduction
+from link_shortening.models import Link
 
 # Create your tests here.
+
+
+class RandomTest(TestCase):
+    @staticmethod
+    def test_reduction():
+        url = reduction()
+        assert len(url) == 28
+
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.url = Link(
+            url='https://www.onliner.by/',
+            abbreviated_url='http://127.0.0.1:8000/oUgolG')
+
+    def test_home(self):
+        result = self.client.get('/')
+        self.assertEqual(result.status_code, 200)
+
+    # Я так и не понял, как проверить вью с редиректорм
+    # Объясни, если можно
+    def test(self):
+        result = self.client.get(self.url.url)
+        self.assertEqual(result.status_code, 200)
